e87813e97a13271bff631d93003a84b6bc9d3f9a
Robert Richardson <rrichardsonv+github@gmail.com>
2017-02-05 11:33:30 -0600

    cleaned it up and added new tab icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class MyApp {
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      Ng2MapComponent['apiUrl']= 'https://maps.google.com/maps/api/js?key=AIzaSyBavd4lIbm2bKkBHwvkMASXCQ3JHHl6FaM&style=element:labels%7Cvisibility:off&style=element:labels.icon%7Cvisibility:off&style=feature:administrative%7Celement:geometry.fill%7Cvisibility:off&style=feature:administrative%7Celement:geometry.stroke%7Ccolor:0x000000%7Cvisibility:on&style=feature:landscape%7Celement:geometry.fill%7Ccolor:0x30ff00%7Cvisibility:on&style=feature:landscape%7Celement:geometry.stroke%7Ccolor:0x30ff00%7Cvisibility:on&style=feature:poi%7Celement:geometry.fill%7Cvisibility:off&style=feature:poi%7Celement:geometry.stroke%7Cvisibility:off&style=feature:road%7Ccolor:0xe1ff8d%7Cvisibility:on&style=feature:road%7Celement:labels%7Cvisibility:off&style=feature:transit%7Celement:geometry.fill%7Ccolor:0xe1ff8d&style=feature:transit%7Celement:geometry.stroke%7Ccolor:0x602400%7Cvisibility:on%7Cweight:1.5&style=feature:water%7Celement:geometry.fill%7Ccolor:0x0e38e4%7Cvisibility:on&style=feature:water%7Celement:geometry.stroke%7Ccolor:0x0e38e4%7Cvisibility:on';
+      Ng2MapComponent['apiUrl']= 'https://maps.google.com/maps/api/js?key=AIzaSyBavd4lIbm2bKkBHwvkMASXCQ3JHHl6FaM';
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
