a786e0d9f87452e60c59fe08853e4085075a6b2e
Dereck Miller <email": null>
2017-02-05 17:47:43 -0600

    Change about pages to map pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
+import { MapPage } from '../pages/map/map';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { RegisterPage } from '../pages/register/register'
@@ -12,7 +12,7 @@ import { Ng2MapModule } from 'ng2-map';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    MapPage,
     ContactPage,
     HomePage,
     RegisterPage,
@@ -25,7 +25,7 @@ import { Ng2MapModule } from 'ng2-map';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    MapPage,
     ContactPage,
     HomePage,
     RegisterPage,
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -3,10 +3,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-map',
+  templateUrl: 'map.html'
 })
-export class AboutPage {
+export class MapPage {
   public mapOptions = {
   zoom: 10,
   mapTypeControl: false,
