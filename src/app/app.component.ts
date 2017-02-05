ad3581584f82d8bb8e92b4ce94363cdd653fe212
Robert Richardson <rrichardsonv+github@gmail.com>
2017-02-05 10:27:37 -0600

    got syled map to work defaulting to current location

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,104 +12,12 @@ import { Ng2MapComponent } from 'ng2-map';
 })
 export class MyApp {
   rootPage = TabsPage;
-  public mapCenter = 'Chicago, IL';
-  public mapOptions = {
-  styles: [
-  {
-    elementType: "labels",
-    stylers: [{ visibility: "off"}]
-  },
-  {
-    elementType: "labels.icon",
-    stylers: [{ visibility: "off" }]
-  },
-  {
-    featureType: "administrative",
-    elementType: "geometry.fill",
-    stylers: [{ visibility: "off" }]
-  },
-  {
-    featureType: "administrative",
-    elementType: "geometry.stroke",
-    stylers: [
-      { color: "#000000" },
-      { visibility: "on" }
-    ]
-  },
-  {
-    featureType: "landscape",
-    elementType: "geometry.fill",
-    stylers: [
-      { color: "#30ff00" },
-      { visibility: "on" }
-    ]
-  },
-  {
-    featureType: "landscape",
-    elementType: "geometry.stroke",
-    stylers: [
-      { color: "#30ff00" },
-      { visibility: "on" }
-    ]
-  },
-  {
-    featureType: "poi",
-    elementType: "geometry.fill",
-    stylers: [{ visibility: "off" }]
-  },
-  {
-    featureType: "poi",
-    elementType: "geometry.stroke",
-    stylers: [{ visibility: "off" }]
-  },
-  {
-    featureType: "road",
-    stylers: [
-      { color: "#e1ff8d" },
-      { visibility: "on" }
-    ]
-  },
-  {
-    featureType: "road",
-    elementType: "labels",
-    stylers: [{ visibility: "off" }]
-  },
-  {
-    featureType: "transit",
-    elementType: "geometry.fill",
-    stylers: [{ color: "#e1ff8d" }]
-  },
-  {
-    featureType: "transit",
-    elementType: "geometry.stroke",
-    stylers: [
-      { color: "#602400" },
-      { visibility: "on" },
-      { weight: 1.5 }
-    ]
-  },
-  {
-    featureType: "water",
-    elementType: "geometry.fill",
-    stylers: [
-      { color: "#0e38e4" },
-      { visibility: "on" }
-    ]
-  },
-  {
-    featureType: "water",
-    elementType: "geometry.stroke",
-    stylers: [
-      { color: "#0e38e4" },
-      { visibility: "on" }
-    ]
-  }
-]};
+
 
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
-      Ng2MapComponent['apiUrl']= 'https://maps.google.com/maps/api/js?key=AIzaSyBavd4lIbm2bKkBHwvkMASXCQ3JHHl6FaM';
+      Ng2MapComponent['apiUrl']= 'https://maps.google.com/maps/api/js?key=AIzaSyBavd4lIbm2bKkBHwvkMASXCQ3JHHl6FaM&style=element:labels%7Cvisibility:off&style=element:labels.icon%7Cvisibility:off&style=feature:administrative%7Celement:geometry.fill%7Cvisibility:off&style=feature:administrative%7Celement:geometry.stroke%7Ccolor:0x000000%7Cvisibility:on&style=feature:landscape%7Celement:geometry.fill%7Ccolor:0x30ff00%7Cvisibility:on&style=feature:landscape%7Celement:geometry.stroke%7Ccolor:0x30ff00%7Cvisibility:on&style=feature:poi%7Celement:geometry.fill%7Cvisibility:off&style=feature:poi%7Celement:geometry.stroke%7Cvisibility:off&style=feature:road%7Ccolor:0xe1ff8d%7Cvisibility:on&style=feature:road%7Celement:labels%7Cvisibility:off&style=feature:transit%7Celement:geometry.fill%7Ccolor:0xe1ff8d&style=feature:transit%7Celement:geometry.stroke%7Ccolor:0x602400%7Cvisibility:on%7Cweight:1.5&style=feature:water%7Celement:geometry.fill%7Ccolor:0x0e38e4%7Cvisibility:on&style=feature:water%7Celement:geometry.stroke%7Ccolor:0x0e38e4%7Cvisibility:on';
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
