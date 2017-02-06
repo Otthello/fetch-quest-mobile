f257ab3c74be1026753f736d250f43a4b31e689a
Dereck Miller <email": null>
2017-02-05 18:04:50 -0600

    initial create quest page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { RegisterPage } from '../pages/register/register'
+import { RegisterPage } from '../pages/register/register';
+import { QuestPage } from '../pages/quest/quest';
 import { TabsPage } from '../pages/tabs/tabs';
 import { Ng2MapModule } from 'ng2-map';
 
@@ -16,6 +17,7 @@ import { Ng2MapModule } from 'ng2-map';
     ContactPage,
     HomePage,
     RegisterPage,
+    QuestPage,
     TabsPage
   ],
   imports: [
@@ -29,6 +31,7 @@ import { Ng2MapModule } from 'ng2-map';
     ContactPage,
     HomePage,
     RegisterPage,
+    QuestPage,
     TabsPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
---
+++ src/pages/quest/quest.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-quest',
+  templateUrl: 'quest.html'
+})
+export class QuestPage {
+  
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
