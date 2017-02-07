71d115acd523ebbe1e314330a37529200fdd2ce9
Dereck Miller <email": null>
2017-02-07 14:16:52 -0600

    basic functionality for adding a quest

--- src/pages/quest_form/quest_form.ts
+++ src/pages/quest_form/quest_form.ts
@@ -1,7 +1,7 @@
 import { Component, NgZone } from '@angular/core';
-import { RequestService } from '../../../app/services/request.service';
 import { NavController } from 'ionic-angular';
 import { ViewController } from 'ionic-angular';
+import { QuestPage } from '../quest/quest';
 
 
 @Component({
@@ -9,35 +9,60 @@ import { ViewController } from 'ionic-angular';
   templateUrl: 'quest_form.html'
 })
 export class QuestFormPage {
-  // task: string;
-  // autocompleteItems;
-  // autocomplete;
-  // service = new google.maps.places.AutocompleteService();
+  task: string;
+  address;
+  autocompleteItems;
+  autocomplete;
+  autocompleteSelection;
+  showList: boolean;
+  // details_service = new
+  service = new google.maps.places.AutocompleteService();
+
+
   constructor (public viewCtrl: ViewController, private zone: NgZone) {
-    // this.autocompleteItems = [];
-    // this.autocomplete = {
-    //   query: ''
-    // };
+    this.address = {
+      place: ''
+    };
+    this.autocompleteItems = [];
+    this.autocomplete = {
+      query: ''
+    };
   }
- //  dismiss() {
- //    this.viewCtrl.dismiss();
- //  }
- //  chooseItem(item: any) {
- //    this.viewCtrl.dismiss(item);
- //  }
- //  updateSearch() {
- //   if (this.autocomplete.query == '') {
- //     this.autocompleteItems = [];
- //     return;
- //   }
- //   let me = this;
- //   this.service.getPlacePredictions({ input: this.autocomplete.query, componentRestrictions: {country: 'USA'} }, function (predictions, status) {
- //     me.autocompleteItems = [];
- //     me.zone.run(function () {
- //       predictions.forEach(function (prediction) {
- //         me.autocompleteItems.push(prediction.description);
- //       });
- //     });
- //   });
- // }
+  dismissPage(){
+    var data = {
+      task: this.task,
+      place_id: this.autocompleteSelection.place_id
+    }
+    if(data.place_id != null){
+      this.viewCtrl.dismiss(data);
+    }
+  }
+  chooseItem(item: any) {
+    // this.viewCtrl.dismiss(item);
+    // var autocomplete = new google.maps.places.Autocomplete(item.description);
+    // console.log(autocomplete);
+    // console.log(item.getPlace().geometry.location);
+    this.autocomplete.query = item.description;
+    this.autocompleteSelection = item;
+    this.autocompleteItems = [];
+  }
+  updateSearch() {
+    if (this.autocomplete.query == '') {
+      this.autocompleteItems = [];
+      return;
+    }
+    let me = this;
+    this.service.getPlacePredictions({ input: this.autocomplete.query, componentRestrictions: {country: 'USA'} }, function (predictions, status) {
+      me.autocompleteItems = [];
+      console.log(predictions);
+      if(predictions != null){
+        me.zone.run(function () {
+          predictions.forEach(function (prediction) {
+            me.autocompleteItems.push(prediction);
+          });
+        });
+      }
+    });
+  }
+
 }
